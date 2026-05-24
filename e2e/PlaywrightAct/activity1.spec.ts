7973fc9a55614488e85a5bc9f06c9700bd315751
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-05-23 18:00:49 -0600

    Completed act1 specs

--- e2e/PlaywrightAct/activity1.spec.ts
+++ e2e/PlaywrightAct/activity1.spec.ts
@@ -5,27 +5,28 @@ test.describe('Mercado Libre', () =>{
         await page.goto('https://www.mercadolibre.com.mx/');
 
         //page.getByRole()
-        await page.getByRole('link', { name: 'Mercado Libre México - Donde' }).click();
+        await page.getByRole('combobox', { name: 'Ingresa lo que quieras' });
+        await page.getByRole('list').filter({ hasText: 'CategoríasVehí' });
 
         //page.getByTestId()
-        await page.getByTestId('text:main-text');
+        await page.getByTestId('action:understood-button');
+        await page.getByTestId('action:customize-button');
 
         //page.getByAltText()
+        // Looked for the website in inspect to get this information, as it is, it isn't shown in the playwright UI, doesn't work probably because it comes from a carroussel of images
+        // await page.getByAltText('HOT SALE, HASTA 60% OFF'); 
 
         //page.getByText()
-        await page.getByText('Puedes pagar con tarjeta, dé');
-        await page.getByText('Aprovecha este beneficio en');
-        await page.getByText('¿No te gusta? ¡Devuélvelo! En')
-
+        await page.getByText('CategoríasVehí').click(); //Trying to do a click shows an error in console of "Permission denied" :(
 
         //page.getByLabel()
-
+        await page.getByLabel('Ingresa lo que quieras encontrar').fill('sueter'); 
 
         //page.getByPlaceholder()
-
+        await page.getByPlaceholder('Buscar productos, marcas y más…').hover();
 
         //page.getByTitle()
-
+        //Not found
 
     })
 })
